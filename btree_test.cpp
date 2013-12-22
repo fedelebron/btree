8776df2590160c454674cc85e6457e15da36361e
Federico Lebrón <federico.lebron@gmail.com>
2013-12-22 20:26:19 +0000

    Initial checkin of B-tree code, based on CLRS.
    Node splitting is still untested, and the whole notion of "value" of a
    node is kind of ill-defined as of yet.

---
+++ btree_test.cpp
@@ -0,0 +1,28 @@
+#include "btree.hpp"
+#include "gtest/gtest.h"
+
+TEST(BTreeTest, ConstructorDefaultsMakeSense) {
+  btree<2, int, float> b;
+  EXPECT_EQ(b.root->n, 0) << "Empty B-tree root did not have 0 children.";
+  EXPECT_TRUE(b.root->leaf) << "Empty B-tree root was not a leaf.";
+}
+
+TEST(BTreeTest, SearchOnEmptyTree) {
+  btree<2, int, int> b;
+  EXPECT_EQ(b.search(0).first, nullptr);
+}
+
+TEST(BTreeTest, SearchBasic) {
+  btree<2, int, int> b;
+  b.root->n++;
+  auto n = new btree_node<2, int, int>;
+  b.root->c[0].reset(n);
+  b.root->keys[0] = 3;
+
+  EXPECT_EQ(b.search(3).second, 0);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
